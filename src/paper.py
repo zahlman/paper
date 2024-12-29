8ffb3008555124e5c3762d703093159d97d93b8e
Karl Knechtel <zahlman@proton.me>
2024-12-28 21:57:47 -0500

    Proof of concept for installation
    The new code uses Pradyun Gedam's `installer` package to install into a
    specified venv (i.e. in general, not the one Paper is running in) using
    the standard Pip strategy (unpack files from the wheel, copy and make
    script wrappers). It parses the venv's config data to determine
    important paths.
    
    The included `installer` wheel is stripped of Windows-specific wrapper
    exectuables. Windows support will be figured out later, but the plan is
    to have Windows installations of Paper download only the stub needed for
    the current system.
    
    (The final bootstrap installer will probably dynamically grab wheels
    using raw `urllib`, and then delegate to the "core" installer wheel to
    finish installation.)
    
    For now, in testing, the `installer` wheel itself as well as the `src`
    directory need to be on `sys.path` (e.g. by setting `PYTHONPATH`). The
    bootstrap script should ensure everything is in the right place for an
    installed version.
    
    Upcoming tasks:
    
    * Make Paper fetch a wheel from its own wheelhouse for installation
    * Support other installation strategies (hard links, symlinks and .pth
      files referring to the unpacked cache
    * Integrate other functional steps: downloading from PyPI (perhaps
      bootstrapped so that `requests` etc. isn't needed in the zipapp),
      building via `build` and resolving dependencies via `resolvelib`

---
+++ src/paper.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from installer import destinations, install, sources
+VenvDestination = destinations.SchemeDictionaryDestination
+WheelFile = sources.WheelFile
+
+
+def _parse_config_line(line):
+    k, _, v = line.partition('=')
+    return k.strip(), v.strip()
+
+
+def _parse_config(path):
+    with open(path / 'pyvenv.cfg') as f:
+        config = dict(map(_parse_config_line, f))
+    return Path(config['executable']), Path(config['home'])
+
+
+def _compute_paths(root, interpreter, home):
+    python_prefix = interpreter.parent.parent
+    python_name = interpreter.name # also used as a folder name.
+    # The `sysconfig` module can only tell us about the current running Python,
+    # but we need to grok other venvs besides the one where Paper is installed.
+    # Therefore, we reproduce its logic here.
+    venv_lib = root / 'lib' / python_name
+    return {
+        'stdlib': python_prefix / 'lib' / python_name,
+        'platstdlib': venv_lib,
+        'platlib': venv_lib / 'site-packages',
+        'purelib': venv_lib / 'site-packages',
+        'include': python_prefix / 'include' / python_name,
+        'platinclude': python_prefix / 'include' / python_name,
+        'scripts': root / 'bin',
+        'data': root 
+    }
+
+
+def _get_destination(venv_root):
+    venv_root = Path(venv_root)
+    interpreter, home = _parse_config(venv_root)
+    paths = _compute_paths(venv_root, interpreter, home)
+    # `installer` requires a string for `interpreter` to write a shebang.
+    # We want to use the venv's executable (even if it's a symlink),
+    # not the base one mentioned in the config file.
+    return VenvDestination(paths, str(venv_root / 'bin' / 'python'), 'posix')
+
+
+def demo(source_path, venv_root):
+    metadata = { 'INSTALLER': b'paper 0.1.0' }
+    with WheelFile.open(source_path) as source:
+        install(source, _get_destination(venv_root), metadata)
