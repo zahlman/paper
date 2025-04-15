a3e81aff3e9f7eda02a5ad4cc116e42069ac8987
Karl Knechtel <zahlman@proton.me>
2025-04-15 15:32:32 -0400

    Fix bugs
    The top-level folder also needs to be passed to `process_folder`; the
    code is changed such that the current folder is processed inside the
    `os.walk` loop rather than iterating over subdirectories. This fixes the
    bug and also avoids a nested iteration (via list comprehension);
    processing of contained files and folders is now all-or-nothing.
    
    The documentation comment is updated to match, and a note is added about
    the use of `onerror` vis-a-vis processing functions.

--- rfbs/src/rfbs/setup/_clone.py
+++ rfbs/src/rfbs/setup/_clone.py
@@ -7,36 +7,43 @@ def _visit(process_folder, process_file, src, dst, onerror):
     for path, dirs, files in walk(src, onerror=onerror):
         path = Path(path)
         dstpath = dst / (path.relative_to(src))
-        dirs[:] = [d for d in dirs if process_folder(path / d, dstpath / d)]
+        if not process_folder(path, dstpath):
+            dirs.clear() # ignore child folders
+            continue # and files
         for f in files:
             process_file(path / f, dstpath / f)
             yield path / f
 
 
 # Process a tree of files and folders (Visitor pattern).
-# 
+#
 # process_folder -> an operation to perform on each folder path.
 # process_file -> an operation to perform on each file path.
 # source -> the source location.
 # destination -> a destination.
 # onerror -> per os.walk.
-# 
+#
 # process_folder and process_file get passed pairs of Paths:
 # a Path from the source, and a corresponding Path within the destination.
-# 
-# The source folder tree is walked top-down (using os.walk for maximum
-# compatibility) and each encountered folder or file in turn is processed.
-# If folder processing returns a falsy value, the path walking will not
-# recurse into that folder.
-# 
+#
+# The source folder tree is walked top-down, (using os.walk for maximum
+# compatibility) and each encountered folder or file in turn (including the
+# top-level folder) is processed. If folder processing returns a falsy value,
+# the path walking will not recurse into that folder.
+#
 # Symbolic links are not followed; if this is needed, have the callbacks
 # check for symbolic links and follow them. Per os.walk, symlinks to
 # folders will be processed by process_folder.
-# 
+#
+# If an error handler is provided, it will also be fed any OSErrors raised by
+# the processing functions will also get fed to the error (by os.walk);
+# to avoid this, have those functions catch the error and re-raise as a
+# different type.
+#
 # The contents of the source directory should not be altered (the semantics
 # of this are unspecified for this case). There is no check for overlap
 # between source and destination folders.
-# 
+#
 # Returns a list of processed files (for further processing, if needed).
 def visit(
     process_folder: Callable[[Path, Path], Any],
