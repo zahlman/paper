9413fb51069f37eae11804aa85c608342ffc1b6f
Karl Knechtel <zahlman@proton.me>
2025-06-07 15:02:45 -0400

    Implement new wheel unpacking
    * "Safe" and "unsafe" wheel unpacks are split into two completely
      separate code paths to simplify the logic. "Safe" unpacking is the
      default; it's unclear whether "unsafe" packing will ever be used.
    
    * "Safe" unpacks now go to a temporary directory first, so that if the
      archive contains any invalid paths, any previously extracted contents
      don't pollute the destination folder. The temporary directory contents
      are then moved into place using `shutil.move`.

--- rfbs/src/rfbs/setup/unpack.py
+++ rfbs/src/rfbs/setup/unpack.py
@@ -1,4 +1,7 @@
-from zipfile import ZipFile 
+from pathlib import Path
+from shutil import move
+from tempfile import TemporaryDirectory
+from zipfile import ZipFile
 
 
 def _validate_item(path):
@@ -14,11 +17,30 @@ def _validate_wheel(archive):
         raise ValueError("corrupt wheel entry {}".format(badfile))
 
 
-def unpack_wheel(src, dst, safe=True):
+def unpack_wheel_unsafe(src, dst):
     with ZipFile(src) as archive:
-        if safe:
-            _validate_wheel(archive)
         for path in archive.namelist():
-            if safe:
-                _validate_item(path)
             archive.extract(path, dst)
+
+
+def _unpack_wheel_safe(archive, where):
+    _validate_wheel(archive)
+    for path in archive.namelist():
+        _validate_item(path)
+        archive.extract(path, where)
+
+
+def _move_contents(src, dst):
+    # shutil.move will recurse, but we want to iterate one level down.
+    # That will leave behind the temporary directory for the context manager
+    # to clean up, and avoid an extra folder at the destination.
+    for item in src.iterdir():
+        move(item, dst)
+
+
+def unpack_wheel(src, dst):
+    with ZipFile(src) as archive, TemporaryDirectory() as td:
+        _unpack_wheel_safe(archive, td)
+        # If there were no errors, move to the real destination.
+        # There isn't really a sane way to recover from an error in this part.
+        _move_contents(Path(td), dst)
