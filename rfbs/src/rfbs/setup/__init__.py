be0453e1f09bd25e4f130c98bff2ca6f8e53fe99
Karl Knechtel <zahlman@proton.me>
2025-06-07 14:53:26 -0400

    Drop vendored `installer`
    Removed the vendored `installer` library and corresponding interface
    code, since completely new code will be used to perform the actual
    installation instead. (The `installer` code is bulky and uses
    abstractions that don't mesh well with the rest of the code, and doesn't
    seem designed to support hard-linking or other copying strategies; plus
    the package includes stub executables for Windows installation that
    PAPER will seek to make optional.)
    
    The removed code had also become effectively redundant, especially after
    the previous commit.

--- rfbs/src/rfbs/setup/__init__.py
+++ rfbs/src/rfbs/setup/__init__.py
@@ -1,64 +1,6 @@
 from pathlib import Path
 from ..fetch import download
-from .installer import destinations, install, sources
 from .unpack import unpack_wheel
-VenvDestination = destinations.SchemeDictionaryDestination
-WheelFile = sources.WheelFile
-
-
-def _parse_config_line(line):
-    k, _, v = line.partition('=')
-    return k.strip(), v.strip()
-
-
-def _parse_config(path):
-    with open(str(path / 'pyvenv.cfg')) as f:
-        config = dict(map(_parse_config_line, f))
-    home = Path(config['home'])
-    try:
-        interpreter = Path(config['executable'])
-    except KeyError: # Make a best guess (needed for <=3.10)
-        version = config['version'].split('.')
-        executable_name = 'python' + '.'.join(version[:2])
-        interpreter = home / executable_name
-    return interpreter, home
-
-
-def _compute_paths(root, interpreter, home):
-    python_prefix = interpreter.parent.parent
-    python_name = interpreter.name # also used as a folder name.
-    # The `sysconfig` module can only tell us about the current running Python,
-    # but we need to grok other venvs besides the one where Paper is installed.
-    # Therefore, we reproduce its logic here.
-    venv_lib = root / 'lib' / python_name
-    # `installer` uses `os.path.join` on these values, which fails for Path
-    # instances on 3.5. So we explicitly convert back to str.
-    return {
-        'stdlib': str(python_prefix / 'lib' / python_name),
-        'platstdlib': str(venv_lib),
-        'platlib': str(venv_lib / 'site-packages'),
-        'purelib': str(venv_lib / 'site-packages'),
-        'include': str(python_prefix / 'include' / python_name),
-        'platinclude': str(python_prefix / 'include' / python_name),
-        'scripts': str(root / 'bin'),
-        'data': str(root)
-    }
-
-
-def _get_destination(venv_root):
-    venv_root = Path(venv_root)
-    interpreter, home = _parse_config(venv_root)
-    paths = _compute_paths(venv_root, interpreter, home)
-    # `installer` requires a string for `interpreter` to write a shebang.
-    # We want to use the venv's executable (even if it's a symlink),
-    # not the base one mentioned in the config file.
-    return VenvDestination(paths, str(venv_root / 'bin' / 'python'), 'posix')
-
-
-def setup_wheel(source_path, venv_root):
-    metadata = { 'INSTALLER': b'paper 0.1.0' }
-    with WheelFile.open(str(source_path)) as source:
-        install(source, _get_destination(venv_root), metadata)
 
 
 def _default_cache():
--- rfbs/src/rfbs/setup/installer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A library for installing Python wheels."""
-
-__version__ = "0.4.0"
-__all__ = ["install"]
-
-from ._core import install  # noqa
