c75bd6c403bc9fa0a28e933944b8e31258958c00
Karl Knechtel <zahlman@proton.me>
2025-06-09 23:36:44 -0400

    Fix installation procedure
    The zipapp's bootstrap installer is fixed to match other recent changes.
    It unpacks the bootstrap wheels into a temporary directory, puts all of
    them on `sys.path`, then uses its own code to do a proper installation -
    first copying the wheels into cache, then installing from the cache into
    a virtual environment.
    
    A diagnostic message is also added at the start of the process to
    indicate the base version of Python used for the installation. This will
    also be the Python used to run PAPER commands later.

--- install.py
+++ install.py
@@ -1,65 +1,73 @@
 from pathlib import Path
 import sys
+from tempfile import TemporaryDirectory
 from venv import create as make_venv
 from zipfile import ZipFile
 
 
-_bootstrap_wheels = [
-    # Wheels whose code is needed for the initial install.
-    # Most important last.
-    'rfbs-0.1.0/rfbs-0.1.0-py3-none-any.whl',
-]
-
-
 _paper_packages = [
     # All packages to install the first time.
-    ('paper-ui', '0.1.0'),
+    # FIXME: determine this list from the bootstrap wheels.
+    ('paper_ui', '0.1.0'),
     ('rfbs', '0.1.0'),
     ('resolvelib', '1.1.0'),
+    ('packaging', '25.0')
 ]
 
 
-def _cache_folder():
-    result = Path.home() / '.cache'
-    if (result / 'paper').exists():
-        # Better safe than sorry, for now.
-        raise IOError('Paper cache already found')
-    return result
-
-
-def _unpack_cache(where):
+def _unpack(where):
     with ZipFile(str(Path(sys.argv[0]))) as me:
         for name in me.namelist():
-            if not name.startswith('paper/'):
-                continue
-            me.extract(name, str(where))
+            if name != '__main__.py':
+                me.extract(name, str(where))
 
 
-def _fix_sys_path(paper_cache):
-    for wheel in _bootstrap_wheels:
-        sys.path.insert(0, str(paper_cache / wheel))
+def _fix_sys_path(bootstrap):
+    for wheel in bootstrap.iterdir():
+        sys.path.insert(0, str(bootstrap / wheel))
 
 
-def _do_initial_install(paper_cache):
+def _copy_to_cache(bootstrap):
+    from rfbs.setup import copy_distribution_into_cache
+    for wheel in bootstrap.iterdir():
+        try:
+            copy_distribution_into_cache(wheel)
+        except IOError:
+            pass # keep going but don't overwrite
+
+
+def _environments_path():
+    return Path.home() / '.local' / 'share' / 'paper' / 'environments'
+
+
+def _do_initial_install():
+    from rfbs.env import ensure
+    # Once `setup_from_cache` can handle updating a package already in the
+    # environment, it should naturally become possible to upgrade existing
+    # PAPER installations using the zipapp for a new version.
+    # For now, this is likely to fail...
     from rfbs.setup import setup_from_cache
-    from rfbs.env import create # For the zipapp, it shouldn't already exist.
     # Use the distribution name for the Paper wheel on PyPI.
-    env = Path.home() / '.local' / 'share' / 'paper' / 'paper-ui'
-    create(env)
+    env = _environments_path() / 'paper-ui'
+    ensure(env, base_python=sys.executable)
     for name, version in _paper_packages:
-        setup_from_cache(name, version, env, cache=paper_cache, fetch=False)
+        setup_from_cache(env, name, version, fetch=False)
 
 
 def main():
     try:
-        cache_dir = _cache_folder()
-        _unpack_cache(cache_dir)
-        _fix_sys_path(cache_dir / 'paper')
-        return _do_initial_install(cache_dir / 'paper')
+        with TemporaryDirectory() as tmpdir:
+            tmpdir = Path(tmpdir)
+            _unpack(tmpdir)
+            _fix_sys_path(tmpdir)
+            _copy_to_cache(tmpdir)
+            _do_initial_install()
+        return 0
     except Exception as e:
         print(e)
         return 1
 
 
 if __name__ == '__main__':
+    print("Installing PAPER 0.1.0, using Python located at", sys.executable)
     sys.exit(main())
